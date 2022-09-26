3aaa6d737ac850e18335c8066a764abf0253769b
Andres Revolledo <arevolledog@gmail.com>
2022-09-26 00:57:45 -0500

    Powershell only linking profile file

--- LinkWindows.ps1
+++ LinkWindows.ps1
@@ -1,4 +1,4 @@
 .\_LinkTemplate.ps1 ~/.config/starship.toml .\starship\starship.toml
 .\_LinkTemplate.ps1 ~/AppData/Local/lvim .\lvim
-.\_LinkTemplate.ps1 ~/Documents/Powershell .\powershell
+.\_LinkTemplate.ps1 ~/Documents/Powershell/Microsoft.PowerShell_profile.ps1 .\powershell\Microsoft.PowerShell_profile.ps1
 
