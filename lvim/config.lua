d06e14375fbdb2c836c18d2693ff5623dc9835fa
Andres Revolledo <arevolledog@gmail.com>
2022-09-25 22:22:21 -0500

    LVim: Moving windows specific config to conditional

--- lvim/config.lua
+++ lvim/config.lua
@@ -6,29 +6,34 @@ lvim.log.level = "warn"
 require("user.keys").config()
 require("user.plugins").config()
 
--- Enable powershell as your default shell
-vim.opt.shell = "pwsh.exe -NoLogo"
-vim.opt.shellcmdflag =
-"-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
-vim.cmd [[
-		let &shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-		let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
-		set shellquote= shellxquote=
+if vim.fn.has('windows') then
+    -- Enable powershell as your default shell
+    vim.opt.shell = "pwsh.exe -NoLogo"
+    vim.opt.shellcmdflag =
+    "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+    vim.cmd [[
+    let &shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+    let &shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+    set shellquote= shellxquote=
   ]]
 
--- Set a compatible clipboard manager
-vim.g.clipboard = {
-    copy = {
-        ["+"] = "win32yank.exe -i --crlf",
-        ["*"] = "win32yank.exe -i --crlf",
-    },
-    paste = {
-        ["+"] = "win32yank.exe -o --lf",
-        ["*"] = "win32yank.exe -o --lf",
-    },
-}
+    -- Set a compatible clipboard manager
+    vim.g.clipboard = {
+        copy = {
+            ["+"] = "win32yank.exe -i --crlf",
+            ["*"] = "win32yank.exe -i --crlf",
+        },
+        paste = {
+            ["+"] = "win32yank.exe -o --lf",
+            ["*"] = "win32yank.exe -o --lf",
+        },
+    }
+end
 
-vim.o.guifont = "FuraCode NF Retina"
+if vim.fn.exists("g:neovide") then
+    vim.o.guifont = "FuraCode NF Retina"
+end
+
 
 -- general
 
